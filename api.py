6aff150fdae7373150c93a62c367ce8a1796a9a2
omkardhanawade-ux <omkardhanawade09@gmail.com>
2024-12-21 23:29:00 +0530

    all files

--- api.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
-print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
-print("PINECONE_API_KEY:", os.getenv("PINECONE_API_KEY"))
-print("PINECONE_ENVIRONMENT:", os.getenv("PINECONE_ENVIRONMENT"))
