28d1a63972b90a166a1eadd4ac29cc38890b7d99
omkardhanawade-ux <omkardhanawade09@gmail.com>
2025-02-26 13:49:32 +0530

    New Commit

--- src/manager/load_manager.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-def load_pdf_manager(file_path):
-    loader = PyPDFLoader(file_path)
-    docs = loader.load()
-
-    return do
--- src/utils/chunk_utils.py
+++
@@ -1,7 +0,0 @@
-def split_text_into_chunks(text, chunk_size=512, overlap=50):
-    words = text.split()
-    chunks = []
-    for i in range(0, len(words), chunk_size - overlap):
-        chunk = " ".join(words[i:i + chunk_size])
-        chunks.append(chunk)
-    return chunks
